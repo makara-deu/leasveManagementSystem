fc786f882d2c16f976cb8913a6b93dc15681a23e
makara deu <makara.deu99@gmail.com>
2020-07-14 22:26:58 +0700

    Up data menu

--- leaveManagementSystem/resources/views/layouts/app.blade.php
+++ leaveManagementSystem/resources/views/layouts/app.blade.php
@@ -29,11 +29,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -52,8 +50,6 @@
                             <a class="navbar-brand" href="" onfocus="myFunction(this)">
                                 {{ __('Department') }}
                             </a>
-                       
-                        
                     </ul>
 
                     <!-- Right Side Of Navbar -->
